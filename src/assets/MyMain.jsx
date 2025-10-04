114b06c6878d1f9526dd9cc8dbd285680058988a
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-04 11:41:31 +0200

    Macrostruttura griglia Main

--- src/assets/MyMain.jsx
+++ src/assets/MyMain.jsx
@@ -1,7 +1,12 @@
 function MyMain () {
     return(
         <>
-
+    <div className="containerMain">
+        <div className="item1"></div>
+        <div className="item2"></div>
+        <div className="item3"></div>
+        <div className="item4"></div>
+    </div>
         </>
     )
 }
