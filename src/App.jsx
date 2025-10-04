a41c53fdd81765cbe75653f881935e45a5ff5b24
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-04 10:28:13 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <MyHeader />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/MyHeader.jsx
@@ -0,0 +1,16 @@
+function MyHeader = () => {
+    return(
+        <>
+            <header>
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <button>Units </button>
+                </div>
+            </header>
+        
+        
+        </>
+    )
+}
