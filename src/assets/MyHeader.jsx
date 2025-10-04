db5e76c01afdcf14729d84537779fc9f30e31651
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-04 15:11:49 +0200

    Creazione Menù a tendina Header

--- src/assets/MyHeader.jsx
+++ src/assets/MyHeader.jsx
@@ -1,14 +1,43 @@
-function MyHeader () {
-    return(
-        <>
-            <header>
-                <div className="containerHeader">
-                    <img src="../../public/img/logo.svg" alt="Logo applicazione"/>
-                    <button className="btnUnits"><i class="fa-solid fa-gear"></i> Units <i class="fa-solid fa-caret-down"></i> </button>
-                </div>
-            </header>
-        </>
-    )
+import { useState } from "react";
+
+import Button from "../assets/components/Button";
+
+function MyHeader() {
+  const [activeButton, setActiveButton] = useState(false);
+
+  function toggleMenu() {
+    setActiveButton((prev) => !prev);
+  }
+
+  return (
+    <>
+      <header>
+        <div className="containerHeader">
+          <img src="../../public/img/logo.svg" alt="Logo applicazione" />
+          <Button
+          isActive={activeButton}
+          onClick={toggleMenu}
+          customClass="btnUnits tue"/>
+          {activeButton && (
+            <ul className="headerdropdownMenu">
+              <h5>Switch to Imperial</h5>
+              <span>Temperature</span>
+              <li>Celsius (°C)</li>
+              <li>Fahrenheit (°F)</li>
+              <hr />
+              <>Wind Speed</>
+              <li>km/h</li>
+              <li>mph</li>
+              <hr />
+              <span>Precipitation</span>
+              <li>Miilimeters (mm)</li>
+              <li>Inches(in)</li>
+            </ul>
+          )}
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default MyHeader;
