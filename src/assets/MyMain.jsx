d5dbf1c5e4bb6944bf50bec142131496e3e307ab
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-04 11:59:25 +0200

    Creazione cella Today Bozza

--- src/assets/MyMain.jsx
+++ src/assets/MyMain.jsx
@@ -1,11 +1,21 @@
+import Today from "./components/Today"
+
 function MyMain () {
     return(
         <>
     <div className="containerMain">
-        <div className="item1"></div>
-        <div className="item2"></div>
-        <div className="item3"></div>
-        <div className="item4"></div>
+        <div className="item1">
+            <Today />
+        </div>
+        <div className="item2">
+
+        </div>
+        <div className="item3">
+
+        </div>
+        <div className="item4">
+
+        </div>
     </div>
         </>
     )
---
+++ src/assets/components/Today.jsx
@@ -0,0 +1,17 @@
+function Today() {
+  return (
+    <>
+      <div className="containerToday">
+        <div>
+          <h4>Berlin, Germany</h4>
+          <span>Tuesday, Aug 5, 2025</span>
+        </div>
+        <div>
+            68°
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Today;
